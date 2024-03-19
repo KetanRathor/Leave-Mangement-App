b2e3383ed86ea9a525bae302fc9725d644fdecb7
ketanAys <ketan.pathtoit@ayssoftwaresolution.com>
2024-03-19 16:46:18 +0000

    backend structure finalised

--- leave_management_backend/src/app.module.ts
+++ leave_management_backend/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
+import { EmployeeModule } from './employee/employee.module';
+import { ManagerModule } from './manager/manager.module';
+import { AdminModule } from './admin/admin.module';
 
 @Module({
-  imports: [],
+  imports: [DatabaseModule, EmployeeModule, ManagerModule, AdminModule],
   controllers: [AppController],
   providers: [AppService],
 })
