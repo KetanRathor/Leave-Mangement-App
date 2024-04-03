f4ebd424d8defd2dd1e37fa89657c8653a2099cc
shruti-88 <shruti.bagde@ayssoftwaresolution.com>
2024-04-03 18:41:12 +0530

    main file

--- leave_management_backend/src/main.ts
+++ leave_management_backend/src/main.ts
@@ -6,13 +6,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-<<<<<<< HEAD
-<<<<<<< HEAD
-  await app.listen(process.env.SERVER_PORT);
-=======
-  await app.listen(3002);
->>>>>>> dd2948662f028272129db33bba63a72733cad706
-=======
+
   const config = new DocumentBuilder()
     .setTitle('Leave Management API')
     .setDescription('leave management API description')
@@ -22,6 +16,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
   await app.listen(3000);
->>>>>>> shruti
+
 }
 bootstrap();
