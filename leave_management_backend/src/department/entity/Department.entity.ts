60ab59b3b738c8e6d6795ff48c4b9e5e0238e902
ketanAys <156405213+ketanAys@users.noreply.github.com>
2024-04-05 18:40:08 +0530

    Swager commented

--- leave_management_backend/src/department/entity/Department.entity.ts
+++ leave_management_backend/src/department/entity/Department.entity.ts
@@ -1,18 +1,18 @@
 import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
 import { Employee } from '../../employee/entities/Employee.entity';
-import { ApiProperty } from '@nestjs/swagger';
+// import { ApiProperty } from '@nestjs/swagger';
 
 @Entity('department')
 export class Department {
-  @ApiProperty({
-    description:'The department id '
-  })
+  // @ApiProperty({
+  //   description:'The department id '
+  // })
   @PrimaryGeneratedColumn()
   department_id: number;
 
-  @ApiProperty({
-    description:'The department name '
-  })
+  // @ApiProperty({
+  //   description:'The department name '
+  // })
   @Column({ nullable: false })
   department_name: string;
 
