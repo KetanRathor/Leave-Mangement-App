30eb8ae86b73650463cd17bdc8291246b64cb9a3
KetanRathor <ketan.pathtoitayssoftwaresolution.com>
2024-04-14 19:26:19 +0530

    Employee profileupload done remaining leave done work from home leave done annual leave done  holiday update delete remaining

--- leave_management_backend/src/leave_types_and_requests/leave_types_and_requests.service.ts
+++ leave_management_backend/src/leave_types_and_requests/leave_types_and_requests.service.ts
@@ -17,8 +17,8 @@ import { HolidaysService } from 'src/holidays/holidays.service';
 @Injectable()
 export class LeaveTypesAndRequestsService {
   private readonly leaveTypes = [
-    { leave_type_id: 1, leave_type_name: 'full', default_balance: 21},
-    { leave_type_id: 4, leave_type_name: 'work from home', default_balance: 10},
+    { leave_type_id: 1, leave_type_name: 'full', default_balance: 21 },
+    { leave_type_id: 4, leave_type_name: 'work from home', default_balance: 10 },
   ];
   // updateStatus(leave_request_id: number, status: string): any {
   //   throw new Error('Method not implemented.');
@@ -29,7 +29,7 @@ export class LeaveTypesAndRequestsService {
     private readonly holidaysService: HolidaysService,
     // @InjectRepository(LeaveType)
     // private readonly leaveTypeRepository: Repository<LeaveType>,
-  ) {}
+  ) { }
 
   async createRequest(
     createLeaveDto: CreateLeaveTypesAndRequestDto,
@@ -62,11 +62,11 @@ export class LeaveTypesAndRequestsService {
   async updateStatus(
     leave_request_id: number,
     status: string,
-    req_mail:string,
+    req_mail: string,
   ): Promise<LeaveRequest> {
     const leaveRequest = await this.findOne(leave_request_id);
     leaveRequest.status = status;
-    leaveRequest.updated_by=req_mail;
+    leaveRequest.updated_by = req_mail;
     return this.leaveRequestRepository.save(leaveRequest);
   }
   async getLeaveRequest(id: number): Promise<LeaveRequest> {
@@ -76,107 +76,6 @@ export class LeaveTypesAndRequestsService {
     }
     return leaveRequest;
   }
-
-  // async getBalanceLeaves(emp_id: number, leave_type_id: number): Promise<number> {
-  //   const leaveType = await this.leaveTypeRepository.findOneBy({ leave_type_id });
-  //   if (!leaveType) {
-  //     throw new Error('Invalid leave type');
-  //   }
-
-  //   const approvedRequests = await this.leaveRequestRepository.find({
-  //     where: {
-  //       leave_type_id: leave_type_id,
-  //       status: 'approved',
-  //       emp_id: emp_id
-  //     },
-  //     relations: ['employee'],
-  //   });
-
-  //   const employeeRequests = approvedRequests.filter(
-  //     (request) => request.employee.emp_id === emp_id
-  //   );
-
-  //   const totalDaysTaken = employeeRequests.reduce((total, request) => {
-  //     const days = ((new Date(request.start_date)).getTime() - (new Date(request.end_date)).getTime()) / (1000 * 60 * 60 * 24);
-  //     // const days = (request.end_date as Date).getTime() - request.start_date.getTime() / (1000 * 60 * 60 * 24);
-
-  //     return total - days;
-  //   }, 0);
-
-  //   return leaveType.default_balance - totalDaysTaken;
-  // }
-
-  // async getPendingLeaveRequests(): Promise<{ id: number, status: string, }[]> {
-  //   const pendingRequests = await this.leaveRepository.find({ where: { status: 'pending' } });
-  //   return pendingRequests.map(request => ({ id: request.emp_id, status: request.status }));
-  // }
-
-  // async getBalanceLeaves(
-  //   emp_id: number,
-  //   leave_type_id: number,
-  // ): Promise<number> {
-  //   const leaveType = this.leaveTypes.find(
-  //     (type) => type.leave_type_id === leave_type_id,
-  //   );
-  //   if (!leaveType) {
-  //     throw new Error('Invalid leave type');
-  //   }
-
-  //   const approvedRequests = await this.leaveRequestRepository.find({
-  //     where: {
-  //       id: leave_type_id,
-  //       status: 'approved',
-  //       emp_id: emp_id,
-  //     },
-  //     relations: ['employee'],
-  //   });
-
-  //   const employeeRequests = approvedRequests.filter(
-  //     (request) => request.employee.id === emp_id,
-  //   );
-
-  //   const totalDaysTaken = employeeRequests.reduce((total, request) => {
-  //     const days =
-  //       (new Date(request.start_date).getTime() -
-  //         new Date(request.end_date).getTime()) /
-  //       (1000 * 60 * 60 * 24);
-  //     // const days = (request.end_date as Date).getTime() - request.start_date.getTime() / (1000 * 60 * 60 * 24);
-
-  //     return total - days;
-  //   }, 0);
-
-  //   return leaveType.default_balance - totalDaysTaken;
-  // }
-
-
-  // async calculateLeaveBalance(emp_id: number, leaveTypes: any[]) {
-  //   const leaveBalances = [];
-  
-  //   for (const leaveType of leaveTypes) {
-  //     let totalDaysTaken = 0;
-  
-  //     // Simulate fetching approved leave requests (replace with actual data source if needed)
-  //     const approvedRequests = [
-  //       // Example data: replace with actual data from your system
-  //       { leave_type_id: leaveType.leave_type_id, duration: 0.5 }, // First half day
-  //       { leave_type_id: leaveType.leave_type_id, duration: 1 }, // Full day
-  //     ];
-  
-  //     for (const request of approvedRequests) {
-  //       totalDaysTaken += request.duration;
-  //     }
-  
-  //     const remainingBalance = leaveType.default_balance - totalDaysTaken;
-  
-  //     leaveBalances.push({
-  //       leaveTypeName: leaveType.leave_type_name,
-  //       remainingBalance: remainingBalance > 0 ? remainingBalance : 0,
-  //     });
-  //   }
-  
-  //   return leaveBalances;
-  // }
-  
 
   async getEmployeesWithPendingLeaveRequests(): Promise<
     { employeeId: number; employeeName: string }[]
@@ -241,24 +140,24 @@ export class LeaveTypesAndRequestsService {
   //   return leaveTypes;
   // }
 
-  async getRemainingLeave(emp_id: number): Promise<{ leave_type_name: string; remaining_balance: number, total_leave:number }[]> {
+  async getRemainingLeave(emp_id: number): Promise<{ leave_type_name: string; remaining_balance: number, total_leave: number }[]> {
     const approvedRequests = await this.leaveRequestRepository.find({
       where: {
         emp_id,
         status: 'approved',
       },
     });
-  
+
     const fullLeaveType = this.leaveTypes.find((leaveType) => leaveType.leave_type_name === 'full');
     if (!fullLeaveType) {
       throw new Error('Missing leave type: full');
     }
-  
+
     const workFromHomeLeaveType = this.leaveTypes.find((leaveType) => leaveType.leave_type_name === 'work from home');
     if (!workFromHomeLeaveType) {
       throw new Error('Missing leave type: work from home');
     }
-  
+
     const totalFullDaysTaken = approvedRequests.reduce((total, request) => {
       const days = this.calculateDays(request.start_date, request.end_date);
       const isFullDay = request.leave_type === 'full';
@@ -266,16 +165,17 @@ export class LeaveTypesAndRequestsService {
       const leaveTaken = isFullDay ? days : (isFullDay || isHalfDay) ? days / 2 : 0;
       return total + leaveTaken;
     }, 0);
-  
+
     const totalWorkFromHomeDaysTaken = approvedRequests.reduce((total, request) => {
       const days = this.calculateDays(request.start_date, request.end_date);
-      return total + (request.leave_type === 'work from home' ? days : 0); 
+      return total + (request.leave_type === 'work from home' ? days : 0);
     }, 0);
-  
+
     const remainingFullBalance = Math.max(0, fullLeaveType.default_balance - totalFullDaysTaken);
     const remainingWorkFromHomeBalance = Math.max(0, workFromHomeLeaveType.default_balance - totalWorkFromHomeDaysTaken);
-  
-    return [
+    const holidays = await this.holidaysService.getHolidayCounts();
+
+    const result = [
       {
         leave_type_name: 'full',
         remaining_balance: remainingFullBalance,
@@ -285,11 +185,23 @@ export class LeaveTypesAndRequestsService {
         leave_type_name: 'work from home',
         remaining_balance: remainingWorkFromHomeBalance,
         total_leave: workFromHomeLeaveType.default_balance,
-        
-      },
+      }
     ];
+
+    const oneYearHoliday = holidays.total_holidays + fullLeaveType.default_balance;
+    const holidayOver = holidays.recent_holidays + remainingFullBalance;
+
+
+    const holidayCount = {
+      leave_type_name: 'Annual Leave',
+      remaining_balance: holidayOver,
+      total_leave: oneYearHoliday,
+    }
+
+    result.push(holidayCount)
+    return result
   }
-  
+
   private calculateDays(startDate: Date, endDate: Date): number {
     const oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
     const start = new Date(startDate);
