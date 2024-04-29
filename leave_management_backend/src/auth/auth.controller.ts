0b88750b42d5048c0bb92969051a8c4f68890def
shitalt02 <163840003+shitalt02@users.noreply.github.com>
2024-04-28 23:03:46 -0700

    fv

--- leave_management_backend/src/auth/auth.controller.ts
+++ leave_management_backend/src/auth/auth.controller.ts
@@ -8,13 +8,11 @@ import { ResetPasswordDto } from './dto/reset-password.dto';
 import {
   Body,
   Controller,
-  Get,
   HttpCode,
   HttpException,
   HttpStatus,
   Param,
   Post,
-  Request,
   UseGuards,
 } from '@nestjs/common';
 import { ApiBody, ApiCreatedResponse, ApiTags } from '@nestjs/swagger';
@@ -36,31 +34,18 @@ export class AuthController {
   }
 
   @Post('forgotpassword')
-  async forgotPassword(@Body('email') email: string) {
-    @HttpCode(HttpStatus.OK)
-    @Post('login')
-    @ApiCreatedResponse({
-        description:'Get Authentication Token'
-    })
-    login(@Body() authPayload: AuthPayloadDto) {
-        const token = this.authService.validateUser(authPayload);
-        if (!token) throw new HttpException('Invalid Credentials', 401);
-        return token;
-    }
-
-    
-@Post('forgotpassword')
-@ApiCreatedResponse({
-    description:'get otp on given mail',
-})
-@ApiBody({
+  @ApiCreatedResponse({
+    description: 'get otp on given mail',
+  })
+  @ApiBody({
     schema: {
-        type: 'object',
-        properties: {
-            email: { type: 'string' }
-        },}
-})
-async forgotPassword(@Body('email') email: string) {
+      type: 'object',
+      properties: {
+        email: { type: 'string' },
+      },
+    },
+  })
+  async forgotPassword(@Body('email') email: string) {
     try {
       const result = await this.authService.forgotPassword(email);
       return result;
@@ -84,41 +69,24 @@ async forgotPassword(@Body('email') email: string) {
       return { error: error.message };
     }
   }
-@Post('reset-password')
 
-    async resetPasswordWithOTP(@Body() resetPasswordDto: ResetPasswordDto) {
-        try {
-            const { email, otp, newPassword, confirmPassword } = resetPasswordDto;
-            await this.authService.resetPasswordWithOTP(email, otp, newPassword, confirmPassword);
-            return { message: 'Password reset successfully' };
-        } catch (error) {
-            return { error: error.message };
-        }
-    }
+  // @Post('reset-password')
+  // async resetPassword(@Body() resetPasswordDto: ResetPasswordDto) {
+  //     try {
+  //         await this.authService.resetPassword(resetPasswordDto.email, resetPasswordDto.newPassword, resetPasswordDto.confirmPassword);
+  //         return { message: 'Password reset successfully' };
+  //     } catch (error) {
+  //         return { error: error.message };
+  //     }
+  // }
 
-// @Post('reset-password')
-// async resetPassword(@Body() resetPasswordDto: ResetPasswordDto) {
-//     try {
-//         await this.authService.resetPassword(resetPasswordDto.email, resetPasswordDto.newPassword, resetPasswordDto.confirmPassword);
-//         return { message: 'Password reset successfully' };
-//     } catch (error) {
-//         return { error: error.message };
-//     }
-// }
-
-// @Post('verify-otp')
-// async verifyOTP(@Body() resetPasswordDto: ResetPasswordDto) {
-//     try {
-//         await this.authService.verifyOTP(resetPasswordDto.email, resetPasswordDto.otp);
-//         return { message: 'OTP verified successfully' };
-//     } catch (error) {
-//         return { error: error.message };
-//     }
-// }
-
-
-    
-
-
-
+  // @Post('verify-otp')
+  // async verifyOTP(@Body() resetPasswordDto: ResetPasswordDto) {
+  //     try {
+  //         await this.authService.verifyOTP(resetPasswordDto.email, resetPasswordDto.otp);
+  //         return { message: 'OTP verified successfully' };
+  //     } catch (error) {
+  //         return { error: error.message };
+  //     }
+  // }
 }
