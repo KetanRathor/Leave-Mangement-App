364c9aa5b33b89c282bd3449fce3e555044fe43d
shitalt02 <163840003+shitalt02@users.noreply.github.com>
2024-04-30 05:21:44 -0700

    .

--- leave_management_backend/src/holidays/dto/update-holidays.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateHolidaysDto } from './create-holidays.dto';
-
-export class UpdateHolidaysDto extends PartialType(CreateHolidaysDto) {}
